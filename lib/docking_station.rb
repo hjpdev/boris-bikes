456d9249f93cf48a34e55c1370208e5574c2a67c
Harry <harryjp1995@gmail.com>
2018-08-07 18:12:12 +0100

    Adds constant to define capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,8 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
+
   attr_reader :bikes
 
   def initialize
@@ -21,7 +23,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.length == 20
+    @bikes.length == DEFAULT_CAPACITY
   end
 
   def empty?
