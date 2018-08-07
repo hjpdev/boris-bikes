cecf52e9209ea2de82deb807f5c988d8672212f4
Harry <harryjp1995@gmail.com>
2018-08-07 16:34:42 +0100

    Raises errors if no bikes in station or dock full.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,5 @@
+require_relative 'bike'
+
 class DockingStation
   attr_reader :bikes
 
@@ -6,7 +8,13 @@ class DockingStation
   end
 
   def release_bike
+    fail('No bikes in station.') if @bikes.empty?
     @bikes.pop
     Bike.new
+  end
+
+  def dock(bike)
+    raise('Dock full.') if @bikes.length == 10
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,30 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it "Should release a working bike when #release_bike is called" do
-    bike = subject.release_bike
+  it 'Should release a working bike when #release_bike is called' do
+    ds = DockingStation.new
+    ds.dock(Bike.new)
+    bike = ds.release_bike
     expect(bike.is_a?(Bike) && bike.working?).to eq true
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'Should dock a bike when #dock is called' do
+    bike = Bike.new
+    expect(subject.dock(bike).include?(bike)).to eq true
+  end
+
+  it 'Should raise an error when there are no bikes & #release is called' do
+    expect { subject.release_bike }.to raise_error('No bikes in station.')
+  end
+
+  it 'Should not dock any bikes if the station is full' do
+    ds = DockingStation.new
+    10.times { ds.dock(Bike.new) }
+    expect { ds.dock(Bike.new) }.to raise_error('Dock full.')
   end
 end
